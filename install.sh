3b98424ca9d880361596a209d544b3539a3b3c78
Zak Nesler <zak@nesler.dev>
2020-12-20 23:08:16 -0500

    Reorder installation steps

--- install.sh
+++ install.sh
@@ -4,19 +4,16 @@
 # just copy each command and run them individually. This way nothing unexpected
 # happens while installing. Or just run it; what's the worst that can happen?
 
+# Pre-configuration
+export ZSH="${XDG_CONFIG_HOME:-$HOME/.config}/oh-my-zsh"
+export ZSH_CUSTOM="$ZSH/custom"
+
 # Install Xcode command-line tools
 echo 'Installing Xcode Tools'
 echo '---------------------'
 xcode-select --install
 
-# Install oh-my-zsh
-echo 'Installing oh-my-zsh'
-echo '--------------------'
-export ZSH="${XDG_CONFIG_HOME:-$HOME/.config}/oh-my-zsh"
-export ZSH_CUSTOM="$ZSH/custom"
-curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-# OS-specific
+# OS-specific install of GNU Stow
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     # Install GNU Stow
@@ -38,10 +35,10 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 
 fi
 
-# Symlink Stow directories
-echo 'Symlinking Stow directories'
-echo '---------------------------'
-ls -d */ | xargs stow -t $HOME
+# Install oh-my-zsh
+echo 'Installing oh-my-zsh'
+echo '--------------------'
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # Install ZSH Autosuggestions
 echo 'Installing ZSH Autosuggestions'
@@ -59,6 +56,11 @@ echo '-------------------'
 curl -fLo ${XDG_CONFIG_HOME:-$HOME/.config}/vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "Vim Plug installed. Run :PlugInstall in Vim to install plugins."
+
+# Symlink Stow directories
+echo 'Symlinking Stow directories'
+echo '---------------------------'
+ls -d */ | xargs stow -t $HOME
 
 # Install formulae from Brewfile
 echo 'Installing Brew Formulae'
