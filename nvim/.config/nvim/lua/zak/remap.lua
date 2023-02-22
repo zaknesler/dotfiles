bd71ccc228752cbc160a7660ed91a2cddd729d41
Zak Nesler <znesler@itswoop.com>
2023-02-22 12:46:23 -0500

    wip

--- nvim/.config/nvim/lua/zak/remap.lua
+++ nvim/.config/nvim/lua/zak/remap.lua
@@ -22,7 +22,10 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- Disable Q and q
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "q", "<nop>")
+
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
