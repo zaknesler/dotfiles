46925a71e9e4ffa6ed7bfeda8403831e19b6f744
Zak Nesler <zak@nesler.dev>
2023-03-02 10:02:48 -0500

    fix eslint

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -55,17 +55,22 @@ vim.diagnostic.config({
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
+  -- enable formatting for eslint
   if client.name == "eslint" then
-    vim.cmd.LspStop('eslint')
-    return
+    client.server_capabilities.documentFormattingProvider = true
+  end
+
+  -- disable formatting for tsserver
+  if client.name == "tsserver" then
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
   vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
   vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
