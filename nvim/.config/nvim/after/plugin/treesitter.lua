f80942f076431325573fb093064335dfa1a312bc
Zak Nesler <zak@nesler.dev>
2023-05-20 14:36:40 -0400

    use bob for nvim

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "lua", "rust", "json" },
+  ensure_installed = { "javascript", "typescript", "lua", "rust", "json" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
