5b50bb3b083abfc28dcde227b7a78e8e244b5778
Zak Nesler <zak@nesler.dev>
2025-12-06 23:34:32 -0500

    update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,14 @@
+local root_files = {
+  '.luarc.json',
+  '.luarc.jsonc',
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+  '.git',
+}
+
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -11,6 +22,7 @@ return {
     "hrsh7th/nvim-cmp",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
+    "j-hui/fidget.nvim",
   },
 
   config = function()
@@ -18,7 +30,6 @@ return {
       formatters_by_ft = {
       }
     })
-
     local cmp = require('cmp')
     local cmp_lsp = require("cmp_nvim_lsp")
     local capabilities = vim.tbl_deep_extend(
@@ -27,12 +38,14 @@ return {
       vim.lsp.protocol.make_client_capabilities(),
       cmp_lsp.default_capabilities())
 
+    require("fidget").setup({})
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
         "lua_ls",
         "rust_analyzer",
         "gopls",
+        "tailwindcss",
       },
       handlers = {
         function(server_name) -- default handler (optional)
@@ -62,13 +75,25 @@ return {
             capabilities = capabilities,
             settings = {
               Lua = {
-                runtime = { version = "Lua 5.1" },
-                diagnostics = {
-                  globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                }
+                format = {
+                  enable = true,
+                  -- Put format options here
+                  -- NOTE: the value should be STRING!!
+                  defaultConfig = {
+                    indent_style = "space",
+                    indent_size = "2",
+                  }
+                },
               }
             }
           }
+        end,
+        ["tailwindcss"] = function()
+          local lspconfig = require("lspconfig")
+          lspconfig.tailwindcss.setup({
+            capabilities = capabilities,
+            filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte", "heex" },
+          })
         end,
       }
     })
@@ -78,7 +103,7 @@ return {
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require('luasnip').lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
@@ -88,6 +113,7 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
+        { name = "copilot", group_index = 2 },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
       }, {
