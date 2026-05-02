dc80060999677ae8502345731fbda57232f90430
Zak Nesler <zak@nesler.dev>
2026-05-01 20:08:52 -0400

    wip

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,6 +3,9 @@ return {
     "saghen/blink.cmp",
     version = "1.*",
     opts = {
+      fuzzy = {
+        implementation = vim.fn.has("win32") == 1 and "lua" or "prefer_rust",
+      },
       keymap = { preset = "default" },
       appearance = {
         nerd_font_variant = "mono",
