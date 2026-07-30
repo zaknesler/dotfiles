770f7994bbcb7efb2ab90adad77a0e69a7548649
Zak Nesler <zak@nesler.dev>
2026-07-30 10:58:01 -0400

    wip

--- nvim/.config/nvim/lua/config/commands.lua
+++ nvim/.config/nvim/lua/config/commands.lua
@@ -1,5 +1,9 @@
--- Update Mason packages + Lazy plugins
 vim.api.nvim_create_user_command("UpdateAll", function()
-  vim.cmd("MasonUpdateAll")
+  -- Update Mason (LSPs)
+  if not vim.g.minimal then
+    vim.cmd("MasonUpdateAll")
+  end
+
+  -- Update Lazy plugins
   require("lazy").update({ show = false })
 end, { desc = "Update Mason packages and Lazy plugins" })
